68fb48ea6a0e25beb1d924dddc22703f1fb6de7e
Eric Callari <ericcallari1@gmail.com>
2018-12-08 11:07:53 -0800

    initialized

--- challenge_4/client/app.jsx
+++ challenge_4/client/app.jsx
@@ -1,2 +1,25 @@
 
+import React from 'react';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Sah dude. Im an App.</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
